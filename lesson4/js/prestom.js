05f59fc0abccaa7c836fb0442da7ef719e592657
falolaomogbolahan <falolaomogbolahan@gmail.com>
2021-05-15 23:18:42 +0000

    sub

--- lesson4/js/prestom.js
+++ lesson4/js/prestom.js
@@ -1,10 +1,10 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.menu')
+const mainnav = document.querySelector('.tab')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 468) mainnav.classList.remove('responsive')};
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 let last_Update = document.lastModified;
 document.getElementById('last_Update').textContent = last_Update;
