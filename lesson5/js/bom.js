87e08b8548e36663851d8f15070852abb0c2eb64
falolaomogbolahan <falolaomogbolahan@gmail.com>
2021-05-21 01:57:48 +0000

    HIT

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,3 +1,20 @@
-const input = document.querySelector('#favchap');
-const input = document.querySelector('button');
-const input = document.querySelector('.list');
+const fav = document.querySelector('#fav');
+const button = document.querySelector('button');
+const output = document.querySelector('.list');
+
+button.addEventListener('click', () => {
+    if (fav.value != '' ) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement ('button');
+    li.textContent = fav.value;
+    deletebutton.textContent = '❌';
+   li.append(deletebutton);
+   output.append(li); 
+   deletebutton.addEventListener('click', function() {
+      output.removeChild(li); 
+   fav.focus;
+});
+fav.value = '';
+fav.focus;
+}
+  });
