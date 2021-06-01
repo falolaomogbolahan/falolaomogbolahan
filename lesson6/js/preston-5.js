f7cb2eb92ba1d2e2f618384f194dd6db21531bf4
falolaomogbolahan <falolaomogbolahan@gmail.com>
2021-06-01 22:08:07 +0000

    commit

---
+++ lesson6/js/preston-5.js
@@ -0,0 +1,29 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.tab')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+ 
+const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var d = new Date();
+var day = d.getDay(); 
+var date = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+var dateStr = day1[day] + ', ' + date + ' ' + month1[month] + ' ' + year;
+document.getElementById("date").innerHTML = dateStr;
+
+
+
+if (day1[day] == 'Friday') {
+    document.getElementById("announce").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.getElementById("announce").style.display = "block";
+}
+else {
+    document.getElementById("announce").style.display = "none";
+}
+
